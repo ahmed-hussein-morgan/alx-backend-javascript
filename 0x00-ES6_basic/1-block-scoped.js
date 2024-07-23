1c5f0dd4c018d421f8bae201f8be947e3f2a08ca
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-07-23 17:31:21 +0300

    task 01 - try 09

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
+    let task = false;
     let task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      const task2 = false;
+      task = true;
+      task2 = false;
     }
   
     return [task, task2];
