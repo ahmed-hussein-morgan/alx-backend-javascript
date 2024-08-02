99cf72b3e900419d5f042693284ccaf1cbcf2e37
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-08-02 22:29:16 +0300

    task 11 - try 01

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,4 +1,13 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = `{ ${departmentName}: [ ${employees} ] }`;
-  return obj;
+  const employeesList = [];
+  for (const employee of employees) {
+    employeesList.push(String(employee));
+  }
+  // const obj = `{ ${departmentName}: [ ${employeesList} ]}`;
+  // return obj;
+
+  // Construct the object as a string
+  const objStr = `{ "${departmentName}": [${employeesList.map((e) => `"${e}"`).join(', ')}] }`;
+  // Parse the string back into an object
+  return JSON.parse(objStr);
 }
