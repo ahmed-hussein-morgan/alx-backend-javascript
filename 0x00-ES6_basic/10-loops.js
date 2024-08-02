a1928d911804e99b8e387acfddcf6492fd43f39f
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-08-02 17:27:07 +0300

    task 10 - try 04 - fix style rules lint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = array;
   let index = 0;
-  for (const item of array) {
-    array[index] = appendString + item;
+  for (const item of newArray) {
+    newArray[index] = appendString + item;
     index += 1;
   }
 
-  return array;
+  return newArray;
 }
